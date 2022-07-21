6f7fe72c4b0d5d0b20d4613d9f6c183392388fc1
Elias Getachew <eluwest2000@gmail.com>
2022-07-21 18:47:22 +0200

    done with the home page

--- src/components/Home/Index.js
+++ src/components/Home/Index.js
@@ -2,18 +2,26 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './index.scss'
 import LogoTitle from './Logo/logo-e.png'
+import LogoElias from './Logo/My Photo b&w.png'
 
 function Home() {
     return (
         <>
             <div className="container home-page">
                 <div className="text-zone">
-                    <h1>Hello there, <br />I'm<img src={LogoTitle} alt="deceloper" />
+                    <h1>Hi, <br />I'm<img src={LogoTitle} alt="deceloper" />
                         lias <br />
                         Web developer
                     </h1>
                     <h2>Frontend Developer | React Expert </h2>
                     <Link to="/contact" className="flat-button">CONTACT ME</Link>
+                </div>
+                <div className="logo-container">
+                    <img
+                        className="solid-logo"
+                        src={LogoElias}
+                        alt="React,  Developer"
+                    />
                 </div>
             </div>
         </>
