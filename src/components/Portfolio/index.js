74b60bcbcb0adc80fd04060ce71c44d14cc6afe4
Elias Getachew <eluwest2000@gmail.com>
2022-07-24 14:24:27 +0200

    added degree image to the portfolio page

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-//import { Link } from 'react-router-dom'
+
 import './index.scss'
 import Loader from 'react-loaders'
 import MyPhoto from './Logo/DegreeColor.png'
@@ -8,22 +8,10 @@ import Degree from './Logo/Degree.jpg'
 function Portfolio() {
     return (
         <>
-            {/* <div className="container home-page">
+            <div className="container">
                 <div className="text-zone">
-                    <h1>Hi, <br />I'm<img src={LogoTitle} alt="developer" />
-                        lias <br />
-                        Web developer
-                    </h1>
-                    <h2>Frontend Developer | React Expert </h2>
-                    <Link to="/contact" className="flat-button">CONTACT ME</Link>
-                    <Link to="/contact" className="flat-button-2">DOWNLOAD CV</Link>
-                </div> */}
-            <div className="container home-page">
-                <div className="text-zone">
-                    <div class="Degree">
-                        <img src={Degree}
-                            alt="Degree" />
-                    </div>
+                    <h1>Computer Science Degree</h1>
+                    <img className='degree-logo' src={Degree} alt="Degree" />
                 </div>
             </div>
             <div className="logo-container">
