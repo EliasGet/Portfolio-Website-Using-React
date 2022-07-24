6a1dac17a25fac89f89baf59f95c20805c354aef
Elias Getachew <eluwest2000@gmail.com>
2022-07-24 14:34:22 +0200

    updated portfolio page

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -5,12 +5,14 @@ import Loader from 'react-loaders'
 import MyPhoto from './Logo/DegreeColor.png'
 import Degree from './Logo/Degree.jpg'
 
+
 function Portfolio() {
     return (
         <>
             <div className="container">
                 <div className="text-zone">
                     <h1>Computer Science Degree</h1>
+                    <a href="/Degree.jpg"><img className='degree-logo' src={Degree} alt="Degree" /></a>
                     <img className='degree-logo' src={Degree} alt="Degree" />
                 </div>
             </div>
